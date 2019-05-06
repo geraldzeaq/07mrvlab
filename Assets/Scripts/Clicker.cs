3a4468c90236be50d9677ee38fc77b2953c51525
zadem-W10 <saoksgerald9@gmail.com>
2019-05-06 06:32:08 -0500

    cap05 advance

--- Assets/Scripts/Clicker.cs
+++ Assets/Scripts/Clicker.cs
@@ -1,27 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
-//using System.Collections.Generic;
 
-//public class Clicker : MonoBehaviour
-public class Clicker
-{
-    public bool clicked()
-    {
-        return Input.anyKeyDown;
-#if (UNITY_ANDROID || UNITY_IPHONE)
+public class Clicker {
+	public bool clicked() {
+		#if (UNITY_ANDROID || UNITY_IPHONE)
 		return Cardboard.SDK.CardboardTriggered;
-#else
-        return Input.anyKeyDown;
-#endif
-    }
-
-    // Use this for initialization
-    //void Start () {
-    //
-    //}
-
-    // Update is called once per frame
-    //void Update () {
-    //
-    //}
+		#else
+		return Input.anyKeyDown;
+		#endif
+	}
 }
